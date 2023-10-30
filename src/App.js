e46b806e32ee7396e46d4d3d10fde6464ece1629
mikaeel akhtar <dreamabroad83@gmail.com>
2023-10-30 08:06:27 +0530

    hhdfh

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import CoinPage from "./pages/coin";
 import ComparePage from "./pages/compare";
 import WatchlistPage from "./pages/watchlist";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import 'react-toastify/dist/ReactToastify.css';
 import { useEffect } from "react";
 import Footer from "./components/Common/Footer/footer";
 
